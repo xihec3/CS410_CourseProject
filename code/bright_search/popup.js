37a0545ee98175d9a8d49781b6abe71aac601900
xihe <xihec3@macbook.local>
2021-12-08 03:41:59 +0800

    Add search and jump util

--- code/bright_search/popup.js
+++ code/bright_search/popup.js
@@ -1,9 +1,7 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action == "initSearcher") {
-     // Get query and page source.
+     // Get query.
      let query = document.getElementById("query").value;
-     this.pageSource = request.main_page_source;
-     var title = this.pageSource.match(/<title[^>]*>([^<]+)<\/title>/)[1];
 
      // Get BrightSearch options.
      this.with_correction = document.getElementById("enable_auto_correction").checked;
@@ -17,40 +15,114 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
          this.matchers.push(getAutoCorrected(query));
        } 
      }
+     document.getElementById("terms-holder").innerHTML = "Will search for: " + this.matchers.join(", ");
 
-     document.getElementById("debug").innerHTML = query + ", " + this.with_correction + ", " + this.with_stemming + ", " + this.with_synonym + ", " + title;
-  } else if (request.acion = "next") {
-    sendResponse({target: 300, a: this.with_stemming, matchers: this.matchers});
+     this.target = -1;
+
+     sendResponse({matchers: this.matchers});
+
+  }
+
+  else if (request.action == "next") {
+    document.getElementById("message").innerHTML = "";
+
+    this.target += 1;
+
+    if (this.target >= request.total_number) {
+      this.target = request.total_number - 1;
+      document.getElementById("message").innerHTML = "Already last one";
+    }
+
+    if (this.target < 0) {
+      this.target = 0;
+    }
+
+    sendResponse({target: this.target, matchers: this.matchers});
+
+  }
+
+  else if (request.action == "previous") {
+    document.getElementById("message").innerHTML = "";
+
+    this.target -= 1;
+
+    if (this.target < 0) {
+      this.target = 0;
+      document.getElementById("message").innerHTML = "Already first one";
+    }
+
+    sendResponse({target: this.target, matchers: this.matchers});
   }
 });
 
 function initSearch() {
-  // Init by adding an id = "target" element at the beginning of body
-  const tag = document.createElement("p");
-  tag.textContent = "TEST";
-  tag.style.backgroundColor = "yellow";
-
-  document.body.insertBefore(tag, document.body.firstChild);
-
-  // Call init searcher
+  // Call init searcher and insert search anchors
   var main_page_source = document.documentElement.outerHTML;
-  chrome.runtime.sendMessage({action: "initSearcher", main_page_source: main_page_source});
-
-  // Call next for the first time
-  chrome.runtime.sendMessage({action: "next", main_page_source: main_page_source}, (response) => {
+  chrome.runtime.sendMessage({action: "initSearcher", main_page_source: main_page_source}, (response) => {
     console.log(response);
-    // Update document.documentElement.outerHTML;
+    
+    var elements = document.body.getElementsByTagName("*");
+    var leaf_elements = new Array();
 
+    for (var i = 0; i < elements.length; i++) {
+      // console.log(elements[i].nodeName)
+      if (!["A", "P", "LI", "CODE", "H1", "H2", "H3", "H4", "H5", "H6"].includes(elements[i].nodeName)) {
+        continue;
+      }
+      if (elements[i].children.length == 0) {
+        leaf_elements.push(elements[i]);
+      }
+    }
 
-  })
+    response.matchers.forEach((matcher) => {
+      // leaf_elements.forEach((elem) => {console.log(elem.innerHTML)});
+      var re = new RegExp(matcher, "gi");
+      for (var i = 0; i < leaf_elements.length; i++) {
+        if (leaf_elements[i].innerHTML.includes('bs-search-anchors')) {
+          continue;
+        }
+        leaf_elements[i].innerHTML = 
+          leaf_elements[i].innerHTML.replace(re, "<span class='bs-search-anchors' style='background:yellow'>" + matcher + "</span>");
+      }
+    });      
+  });
 }
 
 function handlePrevClick() {
-  document.getElementById("debug").innerHTML = "prev";
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.scripting.executeScript(
+            {target: {tabId: tabs[0].id}, func: 
+
+() => {
+chrome.runtime.sendMessage({action: "previous"}, (response) => {
+    console.log("prev: ", response);
+    if (document.getElementsByClassName("bs-search-anchors").length > response.target) {
+      document.getElementsByClassName("bs-search-anchors")[response.target].scrollIntoView(true);
+    }
+});
+}
+
+            }, () => {});
+  });
 }
 
 function handleNextClick() {
-  document.getElementById("debug").innerHTML = "next";
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.scripting.executeScript(
+            {target: {tabId: tabs[0].id}, func: 
+
+() => {
+total_number = document.getElementsByClassName("bs-search-anchors").length;
+chrome.runtime.sendMessage({action: "next", total_number: total_number}, (response) => {
+    console.log(response);
+    if (document.getElementsByClassName("bs-search-anchors").length > response.target) {
+      document.getElementsByClassName("bs-search-anchors")[response.target].scrollIntoView(true);
+    }
+});
+}
+
+            }, () => {});
+  });
 }
 
 function handleSearchClick() {
